034d5bb89830f9cfa4df780a48cc40f4253de6ed
MrunmaiLokhande <lokhandemrunmai1@gmail.com>
2026-03-02 00:11:52 +0530

    Main Driver Class
    Main file uploads.

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import ui.LoginFrame;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new LoginFrame();
+    }
+}
