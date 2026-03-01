7c6bfc68289f53db718f68d5547956f043c19b13
MrunmaiLokhande <lokhandemrunmai1@gmail.com>
2026-03-02 00:10:08 +0530

    Database Layer
    Connection file uploaded.

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,23 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/college_db";
+    private static final String USER = "root";
+    private static final String PASS = "Root@123";
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
